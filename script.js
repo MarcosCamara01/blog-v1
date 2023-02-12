983e39362875f3e77706bd6c04111c180ee7da30
MarcosCamara01 <marcospenelascamara@gmail.com>
2023-02-12 17:49:45 +0100

    agregué el contenido de la página

---
+++ script.js
@@ -0,0 +1,59 @@
+const body = document.querySelector("body"),
+    nav = document.querySelector("nav"),
+    sidebarOpen = document.querySelector(".sidebarOpen"),
+    siderbarClose = document.querySelector(".siderbarClose");
+
+//   js code to toggle sidebar
+sidebarOpen.addEventListener("click", () => {
+    nav.classList.add("active");
+});
+
+body.addEventListener("click", e => {
+    let clickedElm = e.target;
+
+    if (!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("keepOpen")) {
+        nav.classList.remove("active");
+    }
+});
+
+let mainLocation = window.pageYOffset;
+window.onscroll = function () {
+    let currentOffset = window.pageYOffset;
+    if (mainLocation >= currentOffset) {
+        nav.style.top = '0';
+    } 
+    else {
+        nav.style.transition = 'top 0.3s'
+        nav.style.top = '-100px'
+    }
+    mainLocation = currentOffset;
+}
+
+// nav menu list
+let listElements = document.querySelectorAll('.list__button--click');
+
+listElements.forEach(listElement => {
+    listElement.addEventListener('click', () => {
+
+        listElement.classList.toggle('arrow');
+
+        let height = 0;
+        let menu = listElement.nextElementSibling;
+        if (menu.clientHeight == "0") {
+            height = menu.scrollHeight;
+        }
+
+        menu.style.height = `${height}px`;
+
+    })
+});
+
+// Swiper
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: true,
+    }
+});
